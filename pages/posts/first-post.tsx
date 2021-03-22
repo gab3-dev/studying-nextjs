d1117d2744e1b73519eac7dc6da778a2a6991fb8
Gabriel Henrique <henriquebiel13@hotmail.com>
2021-03-22 01:42:31 -0300

    update-2203

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,14 +1,25 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Sung</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image
+        src="/img/Wallpaper.png" // Route of the image file
+        height={1218} // Desired size with correct aspect ratio
+        width={563} // Desired size with correct aspect ratio
+        alt="Made By @GabeDGD"
+      />
     </>
   )
 }
